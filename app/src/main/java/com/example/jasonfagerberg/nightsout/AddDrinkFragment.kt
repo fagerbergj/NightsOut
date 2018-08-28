d84b8c190551e71916d26d4871734303274c578d
Jason Fagerberg <jasonfagerberg@Jasons-MacBook-Pro.local>
2018-08-27 23:33:31 -0400

    Home layout -recycler done

--- app/src/main/java/com/example/jasonfagerberg/nightsout/AddDrinkFragment.kt
+++ app/src/main/java/com/example/jasonfagerberg/nightsout/AddDrinkFragment.kt
@@ -6,16 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class LogFragment : Fragment() {
+class AddDrinkFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log, container, false)
+        return inflater.inflate(R.layout.fragment_add_drink, container, false)
     }
 
     companion object {
-        fun newInstance(): LogFragment = LogFragment()
+        fun newInstance(): AddDrinkFragment = AddDrinkFragment()
     }
 }
--- app/src/main/java/com/example/jasonfagerberg/nightsout/MainActivity.kt
+++ app/src/main/java/com/example/jasonfagerberg/nightsout/MainActivity.kt
@@ -2,11 +2,47 @@ package com.example.jasonfagerberg.nightsout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    // init fragments
+    val homeFragment = HomeFragment.newInstance()
+    val logFragment = LogFragment.newInstance()
+    val profileFragment = ProfileFragment.newInstance()
+    val addDrinkFragment = AddDrinkFragment.newInstance()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setFragment(homeFragment)
+
+        // bottom nav bar
+        val bottomNavigationView: BottomNavigationView? = findViewById(R.id.bottom_navigation_view)
+
+        bottomNavigationView?.setOnNavigationItemSelectedListener { listener ->
+            if (listener.itemId == R.id.bottom_nav_home) {
+                setFragment(homeFragment)
+                true
+            }else if (listener.itemId == R.id.bottom_nav_log){
+                setFragment(logFragment)
+                true
+            }else if (listener.itemId == R.id.bottom_nav_profile){
+                setFragment(profileFragment)
+                true
+            }else{
+                true
+            }
+        }
     }
+
+    private fun setFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_frame, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
