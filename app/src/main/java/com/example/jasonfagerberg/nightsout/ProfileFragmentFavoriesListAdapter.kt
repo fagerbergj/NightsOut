93f30bf7906897d08b99a6f2560671162e2fce85
Jason Fagerberg <jasonfagerberg@Jasons-MacBook-Pro.local>
2018-08-30 16:37:49 -0400

    profile layout spacing changes

--- app/src/main/java/com/example/jasonfagerberg/nightsout/ProfileFragmentFavoriesListAdapter.kt
+++ app/src/main/java/com/example/jasonfagerberg/nightsout/ProfileFragmentFavoriesListAdapter.kt
@@ -35,7 +35,7 @@ class ProfileFragmentFavoriesListAdapter(private val mContext: Context, drinksLi
             if(holder.favorited){
                 // make the toast
                 val toast = Toast.makeText(mContext, "${holder.name.text} favorited", Toast.LENGTH_SHORT)
-                toast.setGravity(Gravity.CENTER, 0, 350)
+                toast.setGravity(Gravity.CENTER, 0, 450)
                 toast.show()
 
                 holder.image.setImageResource(R.drawable.favorite_white_24dp)
@@ -43,7 +43,7 @@ class ProfileFragmentFavoriesListAdapter(private val mContext: Context, drinksLi
             }else{
                 // make the toast
                 val toast = Toast.makeText(mContext, "${holder.name.text} unfavorited", Toast.LENGTH_SHORT)
-                toast.setGravity(Gravity.CENTER, 0, 350)
+                toast.setGravity(Gravity.CENTER, 0, 450)
                 toast.show()
 
                 holder.image.setImageResource(R.drawable.favorite_border_white_24dp)
