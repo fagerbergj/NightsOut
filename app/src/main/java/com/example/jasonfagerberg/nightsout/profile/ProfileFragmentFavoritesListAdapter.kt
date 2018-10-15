252b74d13600014a43f090372d667ad4f369800b
fagerbergj <fagerbergj1@wit.edu>
2018-10-14 23:16:05 -0400

    profile fav and unfav workd sow

--- app/src/main/java/com/example/jasonfagerberg/nightsout/profile/ProfileFragmentFavoritesListAdapter.kt
+++ app/src/main/java/com/example/jasonfagerberg/nightsout/profile/ProfileFragmentFavoritesListAdapter.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import android.widget.Toast
 import com.example.jasonfagerberg.nightsout.main.Drink
 import com.example.jasonfagerberg.nightsout.R
+import com.example.jasonfagerberg.nightsout.main.MainActivity
 import java.util.ArrayList
 
 class ProfileFragmentFavoritesListAdapter(private val mContext: Context, drinksList: ArrayList<Drink>) :
@@ -37,6 +38,11 @@ class ProfileFragmentFavoritesListAdapter(private val mContext: Context, drinksL
                 // make the toast
                 val toast = Toast.makeText(mContext, "${holder.name.text} favorited", Toast.LENGTH_SHORT)
                 toast.setGravity(Gravity.CENTER, 0, 450)
+                mFavoriteDrinksList.add(position, drink)
+                for (d in (mContext as MainActivity).mDrinksList){
+                    if (d == drink) d.favorited = true
+                }
+                //(mContext as MainActivity).mDatabaseHelper.insertRowInFavoritesTable(drink.name, drink.id)
                 toast.show()
 
                 holder.image.setImageResource(R.drawable.favorite_white_24dp)
@@ -46,6 +52,11 @@ class ProfileFragmentFavoritesListAdapter(private val mContext: Context, drinksL
                 val toast = Toast.makeText(mContext, "${holder.name.text} unfavored", Toast.LENGTH_SHORT)
                 toast.setGravity(Gravity.CENTER, 0, 450)
                 toast.show()
+                for (d in (mContext as MainActivity).mDrinksList){
+                    if (d == drink) d.favorited = false
+                }
+                mFavoriteDrinksList.removeAt(position)
+                //(mContext as MainActivity).mDatabaseHelper.deleteRowsInTable("favorites", "drink_name = \"${drink.name}\"")
 
                 holder.image.setImageResource(R.drawable.favorite_border_white_24dp)
                 //mFavoriteDrinksList.remove(drink)
