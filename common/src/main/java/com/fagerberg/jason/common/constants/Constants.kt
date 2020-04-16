fc47e0da8e91a9879cc2db20ce8d3a65bf271551
fagerbergj <jason42722@gmail.com>
2020-04-16 00:13:25 -0400

    moved a few more constants over

--- common/src/main/java/com/fagerberg/jason/common/constants/Constants.kt
+++ common/src/main/java/com/fagerberg/jason/common/constants/Constants.kt
@@ -1,5 +1,15 @@
 package com.fagerberg.jason.common.constants
 
+import com.fagerberg.jason.common.models.VolumeMeasurement
+
 const val DB_NAME = "nights_out_db.db"
 const val DB_VERSION = 40
-val VOLUME_MEASUREMENTS_METRIC_FIRST = arrayOf("ml", "oz", "beers", "shots", "wine glasses", "pints")
+val VOLUME_MEASUREMENTS = VolumeMeasurement.values().map { it.displayName }
+val VOLUME_MEASUREMENTS_METRIC_FIRST = arrayOf(
+        VolumeMeasurement.ML.displayName,
+        VolumeMeasurement.OZ.displayName,
+        VolumeMeasurement.BEERS.displayName,
+        VolumeMeasurement.SHOTS.displayName,
+        VolumeMeasurement.WINE_GLASSES.displayName,
+        VolumeMeasurement.PINTS.displayName
+)
